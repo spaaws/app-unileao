9fbfa44e146d7ce7c4e676ca3b8a7d471fb19f86
Wellington Feitoza <spaaws@gmail.com>
2021-05-26 13:17:59 -0300

    awe

--- db/connect.php
+++ db/connect.php
@@ -1,19 +1,18 @@
 <?php
-  //function connection() {
+  function connection() {
 
-      // $servername = "localhost";
-      // $username   = "root";
-      // $password   = "";
-      // $dbname     = "oingyrsuk1jbsiga";
+      $servername = "ik1eybdutgxsm0lo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+      $username   = "co8f00xb6adb62fj";
+      $password   = "xaaty6r4puyceze4";
+      $dbname     = "oingyrsuk1jbsiga";
 
       try {
-        //$conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
-        $conn = new PDO("JAWSDB_URL");
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
       } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
       }
- // }
+  }
 ?>
