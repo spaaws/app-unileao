8740d4e0b30bc3356aa09cf10771237d0bf3de4f
Wellington Feitoza <spaaws@gmail.com>
2021-05-26 13:07:03 -0300

    123

--- db/connect.php
+++ db/connect.php
@@ -8,7 +8,7 @@
 
       try {
         //$conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
-        $conn = new PDO(JAWSDB_URL);
+        $conn = new PDO("JAWSDB_URL");
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
